a6c422e973bffddd01723f92869c719db3f0b68a
ticochuck <chucklivilla@gmail.com>
2021-12-02 11:28:01 -0800

    test

---
+++ practice.py
@@ -0,0 +1,12 @@
+data = []
+
+vehicle = input('Do you know your Vehicle Name? \n')
+
+if vehicle == 'y':
+    print('Great, more to come')
+else:
+    print('Ok, lets asks some more questions')
+
+
+# for _ in range(int(input('How many cases: \n'))):
+    
