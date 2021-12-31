ca8c526c0c49c3d26928b27ab8813095097d561d
ticochuck <chucklivilla@gmail.com>
2021-12-31 05:07:46 -0800

    working on keyboard / readchar

--- timer.py
+++ timer.py
@@ -1,5 +1,5 @@
 import time
-
+# import keyboard
 
 def countdown(t):
 
@@ -10,34 +10,42 @@ def countdown(t):
         time.sleep(1)
         t -= 1
 
-    print("Beep beep beep")
-
+    print(f'{mins:02d}:{secs-1:02d} Beep beep beep \n')
 
 def stopwatch():
-    an = input('Press 1 to start the stopwatch. \nPress ctrl + c to stop the stopwatch \n')
+    an = input('Press 1 to start the stopwatch. \n')
     
     if an.lower() == '1':
-        stop = False
+        k = None
         mins, secs = 0, 0
 
-        try:
-            while not stop:
+        while not k:
+            try:
                 secs += 1
                 if secs % 59 == 0:
                     mins += 1
                     secs = 0
                 timer = f'{mins:02d}:{secs:02d}'
-                
-                print(timer, end='\r',)
+                print(f"{timer} -- Press ctrl + C to stop", end='\r')
                 time.sleep(1)
-        except KeyboardInterrupt:
-            print('Timer has stopped')
+                # if keyboard.is_pressed('q'):
+                    # break
+                    
+            except KeyboardInterrupt: 
+                print('Timer has stopped')
+                
             
-        
-a = input('1: Timer \n2: Stopwatch \n')
-if a == '1':
-    t = input('Enter time in seconds:\n')
-    countdown(int(t))
-else:
-    stopwatch()
+            
+a = '1'
+while a == '1' or a == '2':
+    a = input('1: Timer \n2: Stopwatch \n3: Exit\n')
+
+    if a == '1':
+        t = input('Enter time in seconds:\n')
+        countdown(int(t))
+    elif a == '2':
+        stopwatch()
+    else:
+        exit()
+
 
